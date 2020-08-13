adf64a72289378d67832c9820b201f963a9be0f3
iamrajiit <69604336+iamrajiit@users.noreply.github.com>
2020-08-13 12:31:57 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,9 @@ const Discord = require('discord.js');
 const client = new Discord.client9();
 
 
-client.login("NzQzMjAyNTE2MjEyMzgzODcx.XzRPRw.EDXJ6WR7L8JhUfVYSAaOV0e7R8Q");
+
 
 client.on('ready',()=>{
     console.log("This bot is logged in!"):
 });
+client.login("process.env.BOT_TOKEN");
