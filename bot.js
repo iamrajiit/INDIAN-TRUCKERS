b09f16d862d0c441be7f4271df4692cece2ff3d0
iamrajiit <69604336+iamrajiit@users.noreply.github.com>
2020-08-13 11:49:40 +0530

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const client = new Discord.client9();
+
+
+client.login("NzQzMjAyNTE2MjEyMzgzODcx.XzRPRw.EDXJ6WR7L8JhUfVYSAaOV0e7R8Q");
+
+client.on('ready',()=>{
+    console.log("This bot is logged in!"):
+});
